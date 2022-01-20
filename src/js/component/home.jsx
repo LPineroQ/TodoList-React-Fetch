95f6639529feda2595fdb972be47e4ff0da2b93e
Luis Piñero Quintana <lpineroq@gmail.com>
2022-01-20 20:20:09 +0000

    React-TodoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 //create your first component
 const Home = () => {
+	const [listTodo, setListTodo] = useState([]);
+	const [newTodo, setNewTodo] = useState("");
+
+	console.log({ newTodo });
+
+	const handleClick = () => {
+		console.log("Click");
+		/*
+		const newListTodo = [...listTodo, newTodo]; */
+		setListTodo([...listTodo, newTodo]);
+	};
+
+	console.log({ listTodo });
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="col-md-6 offset-md-2">
+			<Form>
+				<Form.Group className="mb-3" controlId="formBasicText">
+					<Form.Label>Tu nombre</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Write here..."
+						onChange={(e) => setNewTodo(e.target.value)}
+					/>
+				</Form.Group>
+				<Button variant="primary" type="button" onClick={handleClick}>
+					Click
+				</Button>
+			</Form>
 		</div>
 	);
 };
